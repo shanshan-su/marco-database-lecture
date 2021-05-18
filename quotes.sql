b1affeae760db40e0e0d79c3ce374a0a1da8932b
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-05-18 11:47:13 -0500

    feat: added database creation and table creation

--- quotes.sql
+++ quotes.sql
@@ -1,9 +1,15 @@
+-- create codeup test db if it doesn't exist and then use it
+CREATE DATABASE IF NOT EXISTS codeup_test_db;
 USE codeup_test_db;
 
-CREATE TABLE quotes (
+-- create quotes table
+CREATE TABLE IF NOT EXISTS quotes (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     author_first_name VARCHAR(50) DEFAULT 'NONE',
     author_last_name  VARCHAR(100) NOT NULL,
     content TEXT NOT NULL,
     PRIMARY KEY (id)
 );
+
+-- try rto run a select on the quotes table
+SELECT * FROM quotes;
