bd8f115f0a49d118b708cfd10e22ca82c1b882a8
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-05-18 20:46:27 -0500

    feat: basic statements lecture completed

---
+++ quotes_seeder.sql
@@ -0,0 +1,9 @@
+USE marco_db;
+
+-- INSERT INTO quotes (author_last_name, content)
+-- VALUES ('Hirsh', 'Another interesting fact.');
+
+INSERT INTO quotes (author_first_name, author_last_name, content)
+VALUES ('Douglas', 'Adams',    'Time is an illusion. Lunchtime doubly so.'),
+       ('Mark',    'Twain',    'Clothes make the man. Naked people have little or no influence on society.'),
+       ('Kurt',    'Vonnegut', 'The universe is a big place, perhaps the biggest.');
